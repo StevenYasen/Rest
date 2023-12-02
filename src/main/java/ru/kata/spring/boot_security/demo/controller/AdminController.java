4542c58ecf3ca6f9e0192f3b3b7b7ee6cb6d6cc8
YasenitskyStepan <yastepan@mail.ru>
2023-12-02 16:33:55 +0300

    selected roles in UI - done

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
-
 import java.security.Principal;
-import java.util.List;
+
 
 @Controller
 @RequestMapping("/admin")
@@ -42,30 +41,24 @@ public class AdminController {
     @GetMapping("/addNewUser")
     public String addNewUser(Model model) {
         model.addAttribute("user", new User());
-//        model.addAttribute("allRoles", roleService.getAllRoles());
-//        model.addAttribute("allUsers", userService.allUsers());
         return "redirect:/admin";
     }
 
     @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("newUser") User user, Model model) {
         userService.saveUser(user);
-//        model.addAttribute("allUsers", userService.allUsers());
         return "redirect:/admin";
     }
 
     @GetMapping("/user")
     public String showUser(@RequestParam("id") Long userId, Model model) {
         model.addAttribute("user", userService.findUserById(userId));
-//        model.addAttribute("allUsers", userService.allUsers());
         return "redirect:/admin";
     }
 
     @GetMapping("/edit")
     public String editUser(@RequestParam("id") Long userId, Model model) {
         model.addAttribute("theUser", userService.findUserById(userId));
-//        model.addAttribute("allRoles", roleService.getAllRoles());
-//        model.addAttribute("allUsers", userService.allUsers());
         return "redirect:/admin";
     }
 
@@ -73,7 +66,6 @@ public class AdminController {
     public String updateUser(@ModelAttribute("user") User user,
                              @PathVariable("id") Long id) {
         userService.updateUser(id, user);
-//        model.addAttribute("allUsers", userService.allUsers());
         return "redirect:/admin";
     }
 }
